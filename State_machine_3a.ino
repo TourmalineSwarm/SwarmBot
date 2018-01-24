59266fe1aba663a42d04fca258265caef953af29
natekrinsky <nathan.krinsky@tufts.edu>
2018-01-24 10:05:20 -0500

    Phase 1A

---
+++ State_machine_3a.ino
@@ -0,0 +1,44 @@
+/*
+  Phase 1A
+  Question 3a
+
+  Creates state machine with the following states: on (red), off (none), run (green), sleep (blue), diagnostic (red).
+
+  modified January 23 2018
+  Elsa Ames, Ryan Friedman, Nate Krinsky
+*/
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  
+}
+
+// red led blinks at 10 Hz
+void on() {
+  
+}
+
+// no led is lit up
+void off() {
+  
+}
+
+// green led flashes twice for duty cycle of 0.5 s
+void run() {
+  
+}
+
+// 
+void sleep() {
+  
+}
+
+void diagnostic() {
+  
+}
+
