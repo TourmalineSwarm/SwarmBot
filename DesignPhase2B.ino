0e2ff3738ad2073dd82628d2eeff899c3f596806
elsa-ames113 <36163986+elsa-ames113@users.noreply.github.com>
2018-02-05 10:29:21 -0500

    Add files via upload

---
+++ DesignPhase2B.ino
@@ -0,0 +1,30 @@
+//Dual Motor Control
+
+//enable pins (PWM)
+const int enablePin1; 
+const int enablePin2;
+
+//control pins (digital)
+const int controlPinA_M1; 
+const int controlPinB_M1;
+const int controlPinA_M2; 
+const int controlPinB_M2; 
+
+#define E1 10
+#define E2 11
+
+#define Mot1_contPinA 8
+#define Mot1_contPinB
+#define Mot2_contPinA
+#define Mot2_contPinB
+
+void setup() 
+{
+  pinMode();
+
+}
+
+void loop() 
+{
+  
+}
